53f2be6a79738b082f931413e0d10f14d2d0d204
Kenward Terhemba <codewithkenward@gmail.com>
2025-09-20 15:29:33 +0100

    add button component

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,9 +7,11 @@ const Landing: React.FC = () => {
       <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
 
+      {/* Multiple button instances */}
       <Button title="Small Rounded" styles="text-sm rounded-sm px-2 py-1" />
       <Button title="Medium Rounded" styles="text-base rounded-md px-4 py-2" />
       <Button title="Large Rounded" styles="text-lg rounded-full px-6 py-3" />
+      <Button title="Custom Rounded" styles="text-lg rounded-lg px-5 py-2" />
     </div>
   )
 }
