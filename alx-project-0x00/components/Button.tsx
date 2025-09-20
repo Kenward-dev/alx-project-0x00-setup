bc82b68c923feb7c8b3bf789c3f9055eb4df4de3
Kenward Terhemba <codewithkenward@gmail.com>
2025-09-20 15:28:25 +0100

    add button component

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,18 +1,11 @@
-import React from "react";
+import { ButtonProps } from "@/interfaces"
 
-interface ButtonProps {
-  title: string;
-  className?: string; // tailwind styles passed in
-}
-
-const Button: React.FC<ButtonProps> = ({ title, className }) => {
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
-    <button
-      className={`px-4 py-2 font-semibold text-white bg-blue-500 hover:bg-blue-600 transition ${className}`}
-    >
+    <button className={`px-4 py-2 bg-blue-500 text-white ${styles}`}>
       {title}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,32 +1,15 @@
 import Card from "@/components/Card"
-import Button from "@/components/Button"  
+import Button from "@/components/Button"
 
 const Landing: React.FC = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-xl font-extralight mb-6">Landing Page</h1>
-      
+    <div className="flex flex-col gap-4 items-center">
+      <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
 
-      <section className="mt-10 space-y-8">
-        <div>
-          <h2 className="text-lg font-medium mb-4">Button Sizes</h2>
-          <div className="flex gap-4">
-            <Button title="Small" className="text-sm px-2 py-1" />
-            <Button title="Medium" className="text-base px-4 py-2" />
-            <Button title="Large" className="text-lg px-6 py-3" />
-          </div>
-        </div>
-
-        <div>
-          <h2 className="text-lg font-medium mb-4">Button Shapes</h2>
-          <div className="flex gap-4">
-            <Button title="Rounded-sm" className="rounded-sm" />
-            <Button title="Rounded-md" className="rounded-md" />
-            <Button title="Rounded-full" className="rounded-full" />
-          </div>
-        </div>
-      </section>
+      <Button title="Small Rounded" styles="text-sm rounded-sm px-2 py-1" />
+      <Button title="Medium Rounded" styles="text-base rounded-md px-4 py-2" />
+      <Button title="Large Rounded" styles="text-lg rounded-full px-6 py-3" />
     </div>
   )
 }
